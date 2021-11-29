67df63eaecf9fb29a5dad3b92f425c47ce27adce
Stephanie Freeman <stephanie.freeman@bryanuniversity.edu>
2021-11-29 13:06:16 -0800

    correcting my mistake

--- week1/Element/Elements.js
+++ week1/Element/Elements.js
@@ -1,6 +1,3 @@
 let element = document.getElementById("idName");
 console.log("idName")
 element.innerHTML += "Hello World";
-let element = document.getElementById("idName");
-console.log("idName")
-element.innerHTML += "HEllo World";
