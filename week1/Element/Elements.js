3f7b427e7420f2443cfb1b2282d3e59111d38155
Stephanie Freeman <stephanie.freeman@bryanuniversity.edu>
2021-11-29 13:04:34 -0800

    changes to HTML

--- week1/Element/Elements.js
+++ week1/Element/Elements.js
@@ -1,3 +1,6 @@
 let element = document.getElementById("idName");
 console.log("idName")
 element.innerHTML += "Hello World";
+let element = document.getElementById("idName");
+console.log("idName")
+element.innerHTML += "HEllo World";
